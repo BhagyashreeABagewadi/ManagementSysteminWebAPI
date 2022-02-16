561b1db38ccd5115ea8b0115d454070fa93f0cd6
Hp <Hp@DESKTOP-V5288M8>
2022-02-16 15:13:07 +0530

    Add project files.

---
+++ ManagementSysteminWebAPI/Models/EFDBhandle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSysteminWebAPI.Models
+{
+    public class EFDBhandle : DbContext
+    {
+        public string conn = "Server=DESKTOP-V5288M8;Database=CustomerDB;Trusted_Connection=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsB)
+        {
+            optionsB.UseSqlServer(conn);
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
